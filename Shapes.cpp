ec549654e537f8c0adb2e7607223cf9c70a9b410
Muhammad Aqeel <129723771+MuhammadAqeel1209@users.noreply.github.com>
2023-04-07 14:46:54 +0500

    Add files via upload

---
+++ Shapes.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+
+using namespace std;
+int main()
+{
+//	for(int i =1; i < 6;i++)
+//	{
+//		for(int j = 0; j < i;j++)
+//		{
+//			cout << "*";
+//		}
+//		cout << endl;
+//	}
+
+//	for(int i =0; i < 5;i++)
+//	{
+//		for(int j = 5; j >=i; j--)
+//		{
+//			cout << "*" ;
+//		}
+//		cout << endl;
+//	}
+
+//	for(int i = 1; i < 6; i++)
+//	{
+//		for( int j = 0; j < i;j++)
+//		{
+//			cout << i ;
+//		}
+//		cout << endl;
+//	}
+
+	for(int i = 1; i<=5;i++)
+	{
+		for(int j = 0; j <= i;j++)
+		{
+			cout << j;
+		}
+		cout << endl;
+	}
+}
